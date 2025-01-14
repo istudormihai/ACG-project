d5be1f69311a0b8f31d918e5062bbabcd6087c13
Mihai-Septimiu Țigăeru <151669209+septimium@users.noreply.github.com>
2025-01-14 20:04:14 +0200

    Update skybox_vertex_shader.glsl

--- PART2 (3D GAME)/GameEngine/GameEngine/Shaders/skybox_vertex_shader.glsl
+++ PART2 (3D GAME)/GameEngine/GameEngine/Shaders/skybox_vertex_shader.glsl
@@ -10,5 +10,5 @@ uniform mat4 view;
 void main() {
     TexCoords = aPos;
     vec4 pos = projection * view * vec4(aPos, 1.0);
-    gl_Position = pos.xyww; // Keep depth as maximum for proper skybox rendering
+    gl_Position = pos.xyww; 
 }
